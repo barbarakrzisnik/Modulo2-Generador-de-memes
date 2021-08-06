02d64b3fe850ca4c0a6a38fef270dcd114675b09
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2021-08-06 11:40:42 -0300

    base modo oscuro js

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const panelImagen = document.getElementById("contenedor-panel-imagen")
 const panelTexto = document.getElementById("contenedor-panel-texto")
 const botonImagen = document.getElementById("nav-boton-imagen")
 const botonTexto = document.getElementById("nav-boton-texto")
+const botonModoOscuro = document.getElementById("nav-boton-modo-oscuro")
 
 
-
+//Elementos para modo oscuro
+const body = document.querySelector("body")
+const header = document.querySelector("header")
 
 
 // Funcionamiento boton Imagen
@@ -19,7 +22,6 @@ const mostrarPanelImagen = () => {
 botonImagen.onclick = mostrarPanelImagen
 
 
-
 // // Funcionamiento boton Texto
 const mostrarPanelTexto = () => {
     panelImagen.classList.add("ocultar-panel")
@@ -27,3 +29,13 @@ const mostrarPanelTexto = () => {
   }
   
 botonTexto.onclick = mostrarPanelTexto
+
+//Funcionamiento boton Modo Oscuro
+const activarModoOscuro = () => {
+    body.classList.toggle("modo-oscuro-gris-claro")
+    header.classList.toggle("modo-oscuro-gris-oscuro")
+    panelTexto.classList.toggle("modo-oscuro-gris-medio")
+    panelImagen.classList.toggle("modo-oscuro-gris-medio")
+}
+
+botonModoOscuro.onclick = activarModoOscuro
